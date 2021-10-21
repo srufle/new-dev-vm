26f0d0a252109a532c916b704e213be653d44117
Stephen Rufle <srufle@gmail.com>
2021-10-20 19:21:11 -0700

    Adding script files

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ansible-playbook -v install.yaml
---
+++ new-ubuntu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+sudo apt-get install python3-distutils python3-apt python3-testresources -y
+sudo apt-get install wget sshpass vim git bash-completion -y
+sudo apt-get install openssh-server -y
+
+rm -f "${HOME}/Downloads/get-pip.py"
+wget -P "${HOME}/Downloads" https://bootstrap.pypa.io/get-pip.py
+python3 "${HOME}/Downloads/get-pip.py"
+
+export PATH=${HOME}/.local/bin:${PATH}
+pip --version
+
+pip install paramiko
+pip install ansible-core
+pip install ansible
+pip install pywinrm
+
+# https://pypi.org/project/argcomplete/#activating-global-completion
+pip install argcomplete
+activate-global-python-argcomplete --user
+sudo ln -s $HOME/.bash_completion.d/python-argcomplete /etc/bash_completion.d/python-argcomplete
+
+
+sudo apt-get install open-vm-tools open-vm-tools-desktop open-vm-tools-dev  -y
+sudo apt-get install xrdp -y
+sudo systemctl enable --now xrdp
+
+# ansible-vault - look in lastpass for password
+# /tmp/vault.pass
